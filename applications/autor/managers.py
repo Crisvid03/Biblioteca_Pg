486724c0195f92e4ccf0b5effcfaff3542721842
Crisvid03 <crisvid03@gmail.com>
2024-07-15 12:12:38 -0600

    ahi suave

--- applications/autor/managers.py
+++ applications/autor/managers.py
@@ -1,6 +1,16 @@
 from django.db import models
 
+#importando para q haga la comparacion OR
+from django.db.models import Q
+
 class AutorManager(models.Manager):
 
-    def listarAutores(self):
-        return self.all()
+    def buscar_autor(self , kword):
+        resultado = self.filter(name__icontains=kword)
+        return resultado
+
+    def buscar_autor2(self , kword):
+        resultado = self.filter(
+            Q(name__icontains=kword) | Q(last_name__icontains=kword)
+        )
+        return resultado
